29d8640a4c4388c95baa74fbfa6a008eeb7305fb
Kevin <Kevin@DESKTOP-1B1GDV2>
2017-10-19 17:14:21 +0200

    TIENS VOILA TA POPUP ENCULE !

---
+++ MaquetteHTML/js/script.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function() {
+    $('.fa-user-circle').click(function (e) { 
+        $('#page_accueil').removeClass('blur-out');
+        $('#page_accueil').addClass('blur-in');
+        $('.pop-up').show();
+        $('.pop-up').fadeIn(1000);
+
+            $('.close-button').click(function (e) { 
+        
+            $('.pop-up').fadeOut(700);
+            $('#page_accueil').removeClass('blur-in');
+            $('#page_accueil').addClass('blur-out');
+            e.stopPropagation();
+
+        });
+    });
+});
+ 
